3b72b63fcdf99c55a0b04b95e7271b6d7eef01ce
Robert Kalaf Jr. <bobby.kalaf@outlook.com>
2026-01-04 05:49:45 -0800

    Add grimoire reminder token manager

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,11 @@
 // src/store/index.ts
 import { configureStore } from '@reduxjs/toolkit';
 import { gameSlice } from './game/game-slice';
+import { grimoireSlice } from './grimoire/grimoire-slice';
 
 const store = configureStore({
     reducer: {
-        game: gameSlice.reducer
+        game: gameSlice.reducer,
+        grimoire: grimoireSlice.reducer
     }
 });
