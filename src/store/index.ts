05f085c4b32691c634ea80b30f9db0d50d9e0416
Robert Kalaf Jr. <bobby.kalaf@outlook.com>
2026-01-04 05:46:43 -0800

    Add storyteller queue slice

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,11 @@
 // src/store/index.ts
 import { configureStore } from '@reduxjs/toolkit';
 import { gameSlice } from './game/game-slice';
+import { storytellerQueueSlice } from './st-queue/st-queue-slice';
 
 const store = configureStore({
     reducer: {
-        game: gameSlice.reducer
+        game: gameSlice.reducer,
+        storytellerQueue: storytellerQueueSlice.reducer
     }
 });
