11724e758f3927710565b23528c60631229210a4
Bobby Kalaf <bobby@outlook.com>
2026-01-04 01:22:54 -0800

    More Parent cleanup

--- src/components/CharacterTokenParent.tsx
+++ src/components/CharacterTokenParent.tsx
@@ -48,62 +48,66 @@ export function CharacterTokenParent({
     const { firstNight, firstNightReminder, otherNight, otherNightReminder, ability } = $$ROLES[role];
     return (
         <>
-            <Tooltip>
-                <TooltipTrigger asChild>
-                    <button
-                        className='absolute grid place-items-center rounded-full focus:outline-none focus:ring-2 focus:ring-ring data-[is-alive=false]:bg-black/50 group'
-                        data-is-alive={isAlive}
-                        data-is-dead={!isAlive}
-                        data-character-type={characterType}
-                        data-alignment={alignment}
-                        data-is-marked={isMarked ?? false}
-                        data-is-drunk={isDrunk ?? false}
-                        data-is-poisoned={isPoisoned ?? false}
-                        style={{ width: tokenSize, height: tokenSize, left: x, top: y }}
-                        type='button'
-                        onClick={() => alert('CLICKED')}
-                    >
-                        {/* <div
+            <button
+                className='absolute grid place-items-center rounded-full focus:outline-none focus:ring-2 focus:ring-ring data-[is-alive=false]:bg-black/50 group'
+                data-is-alive={isAlive}
+                data-is-dead={!isAlive}
+                data-character-type={characterType}
+                data-alignment={alignment}
+                data-is-marked={isMarked ?? false}
+                data-is-drunk={isDrunk ?? false}
+                data-is-poisoned={isPoisoned ?? false}
+                style={{ width: tokenSize, height: tokenSize, left: x, top: y }}
+                type='button'
+                onClick={() => alert('CLICKED')}
+            >
+                {/* <div
                         className='absolute inset-0 h-full w-full rounded-full border-0 ring-0 outline-0 data-[character-type="demon"]:bg-rose-300/80 data-[character-type="minion"]:bg-orange-300/80 data-[character-type="outsider"]:bg-purple-300/80 data-[character-type="townsfolk"]:bg-blue-300/80'
                         data-character-type={characterType}
                     /> */}
-                        <ShadedOverlay />
-                        <img
-                            src={shroudImg}
-                            alt=''
-                            className='absolute top-0 w-1/3 h-2/3 invisible z-30 data-[is-alive=false]:visible'
-                            draggable={false}
-                            data-is-alive={isAlive}
-                        />
-                        <SkullIcon
-                            className='absolute inset-0 w-full h-full object-over pointer-events-none invisible data-[is-marked=true]:visible z-20 opacity-50 group-data-[is-alive=false]:invisible'
-                            data-is-marked={isMarked}
-                        />
-                        {children}
+                <ShadedOverlay />
+                <img
+                    src={shroudImg}
+                    alt=''
+                    className='absolute top-0 w-1/3 h-2/3 invisible z-30 data-[is-alive=false]:visible'
+                    draggable={false}
+                    data-is-alive={isAlive}
+                />
+                <SkullIcon
+                    className='absolute inset-0 w-full h-full object-over pointer-events-none invisible data-[is-marked=true]:visible z-20 opacity-50 group-data-[is-alive=false]:invisible'
+                    data-is-marked={isMarked}
+                />
+                {children}
+                <Tooltip>
+                    <TooltipTrigger asChild>
                         <span className='place-self-center text-center bg-transparent mx-auto transform -translate-y-full absolute w-full top-1/7 z-30 font-black text-black text-base'>
                             <Label className='w-full min-w-fit rounded-md shadow-inner border-black border-2 bg-white text-black text-sm text-center font-bold px-1 py-0.5 justify-center z-30'>
                                 {$$ROLES[role]?.name}
                             </Label>
                         </span>
-                        <FirstNightOrderBadge
-                            order={firstNight}
-                            reminder={firstNightReminder}
-                        />
-                        <OtherNightOrderBadge
-                            order={otherNight}
-                            reminder={otherNightReminder}
-                        />
-                        <Label
-                            className='w-full min-w-fit rounded-md shadow-inner border-white border-2 bg-black text-white text-center text-sm absolute bottom-0 font-bold transform translate-y-1/2 data-[character-type=demon]:bg-red-500 data-[character-type=minion]:bg-orange-500 data-[character-type=outsider]:bg-cyan-500 data-[character-type=townsfolk]:bg-blue-500 data-[character-type=traveler]:bg-yellow-500 px-1.5 py-0.5 justify-center z-30'
-                            htmlFor=''
-                            data-character-type={$$ROLES[role].team}
-                        >
-                            {name}
+                    </TooltipTrigger>
+                    <TooltipContent className='z-40'>
+                        <Label className='bg-slate-700 px-1.5 py-0.5 rounded-lg text-white text-lg text-wrap'>
+                            {ability}
                         </Label>
-                    </button>
-                </TooltipTrigger>
-                <TooltipContent>Ability Text</TooltipContent>
-            </Tooltip>
+                    </TooltipContent>
+                </Tooltip>
+                <FirstNightOrderBadge
+                    order={firstNight}
+                    reminder={firstNightReminder}
+                />
+                <OtherNightOrderBadge
+                    order={otherNight}
+                    reminder={otherNightReminder}
+                />
+                <Label
+                    className='w-full min-w-fit rounded-md shadow-inner border-white border-2 bg-black text-white text-center text-sm absolute bottom-0 font-bold transform translate-y-1/2 data-[character-type=demon]:bg-red-500 data-[character-type=minion]:bg-orange-500 data-[character-type=outsider]:bg-cyan-500 data-[character-type=townsfolk]:bg-blue-500 data-[character-type=traveler]:bg-yellow-500 px-1.5 py-0.5 justify-center z-30'
+                    htmlFor=''
+                    data-character-type={$$ROLES[role].team}
+                >
+                    {name}
+                </Label>
+            </button>
         </>
     );
 }
--- src/components/NightOrderBadge.tsx
+++ src/components/NightOrderBadge.tsx
@@ -1,6 +1,7 @@
 // src/components/NightOrderBadge.tsx
 import { Badge } from 'lucide-react';
 import { Tooltip, TooltipContent, TooltipTrigger } from './ui/tooltip';
+import { Label } from './ui/label';
 
 export function NightOrderBadge({
     order,
@@ -14,7 +15,7 @@ export function NightOrderBadge({
     order: number;
 }) {
     const className = [
-        'absolute  top-1/2 min-w-6 h-6 z-40 transform   text-white font-extrabold text-lg group-data-[is-alive=false]:invisible data-[order="0"]:invisible tabular-nums',
+        'absolute top-1/2 min-w-6 h-6 z-20 transform   text-white font-extrabold text-lg group-data-[is-dead=true]:invisible data-[order="0"]:invisible tabular-nums',
         bgColor,
         side
     ].join(' ');
@@ -25,10 +26,12 @@ export function NightOrderBadge({
                     className={className}
                     data-order={order}
                 >
-                    {order}
+                    <span className='text-white text-base'>{order}</span>
                 </Badge>
             </TooltipTrigger>
-            <TooltipContent>{reminder}</TooltipContent>
+            <TooltipContent>
+                <Label className='bg-slate-700 px-1.5 py-0.5 rounded-lg text-white text-lg text-wrap'>{reminder}</Label>
+            </TooltipContent>
         </Tooltip>
     );
 }
